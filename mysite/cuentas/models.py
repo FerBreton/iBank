8b2a24433b216602531fee92b2c5d61e311ce654
FerBreton <188108@iberopuebla.mx>
2023-03-12 15:21:01 -0600

    model segmentation

--- mysite/cuentas/models.py
+++ mysite/cuentas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from login.models import User
 
 # Create your models here.
+
+class Contact(models.Model):
+    idAcc = models.ForeignKey('Account', on_delete=models.CASCADE, blank=True, null=True, related_name='cuentas')
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    numAccount = models.IntegerField()
+    saldo = models.IntegerField(default=0)
+    pin = models.CharField(max_length=4, default=0000)
+    contact = models.ManyToManyField(Contact)
+    
+    def __str__(self):
+        return str(self.numAccount)
--- mysite/transacciones/models.py
+++ mysite/transacciones/models.py
@@ -1,20 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from cuentas.models import Account
 
 # Create your models here.
-
-class Contact(models.Model):
-    idAcc = models.ForeignKey('Account', on_delete=models.CASCADE, blank=True, null=True, related_name='cuentas')
-
-class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    numAccount = models.IntegerField()
-    saldo = models.IntegerField()
-    pin = models.CharField(max_length=4)
-    contact = models.ManyToManyField(Contact)
-    
-    def __str__(self):
-        return str(self.numAccount)
 
 class Transaction(models.Model):
     TYPES = (
@@ -25,7 +12,7 @@ class Transaction(models.Model):
     amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=20, choices=TYPES)
-    idUser = models.ForeignKey('Account', on_delete=models.CASCADE, blank=True, null=True)
+    idUser = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return str(self.amount)
