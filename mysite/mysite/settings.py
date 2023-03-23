8c3c82c673e720762b3eeb195c1f8e05202b0b2e
FerBreton <188108@iberopuebla.mx>
2023-03-22 19:36:44 -0600

    messages template added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import io
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
