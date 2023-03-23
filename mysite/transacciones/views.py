b608645ce44df6754c65b9700a74f2280ffd3bb7
FerBreton <188108@iberopuebla.mx>
2023-03-22 19:38:28 -0600

    deposit and withdrawal added without validation

--- mysite/transacciones/views.py
+++ mysite/transacciones/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.views import View, generic
 from .forms import TransactionForm
 from .models import Transaction
+from cuentas.models import Account
 from django.contrib import messages
-import datetime
 
 # Create your views here.
 
@@ -21,7 +20,22 @@ class Home(View):
         form = TransactionForm(request.POST)
         if form.is_valid():
             llenado = form.save(commit=False)
-            transactions = Transaction.objects.filter(amount = llenado.amount, timestamp__gte = datetime.date.today(), type = llenado.type)
+            cuentas = Account.objects.all()
+            num = str(llenado.idUser)
+            if llenado.type == 'Deposito':
+                for c in cuentas:
+                    if c.numAccount == int(num):
+                        c.saldo = c.saldo + llenado.amount
+                        c.save()
+            elif llenado.type == "Retiro":
+                for c in cuentas:
+                    if c.numAccount == int(num):
+                        if c.saldo < llenado.amount:
+                            messages.warning(request, 'El saldo es insuficiente, vuelve a intentarlo')
+                            return redirect('transacciones')
+                        else:
+                            c.saldo = c.saldo - llenado.amount
+                            c.save()
             llenado.save()
             messages.success(request, 'Transaccion realizada')
             return redirect('transacciones')
